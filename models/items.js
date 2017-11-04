5a1bb55043692fe930df91fbc1918ce1ea2ccd98
Anmol Nuggehalli <anuggehalli93@gmail.com>
2017-11-04 12:32:44 -0600

    items table

---
+++ models/items.js
@@ -0,0 +1,22 @@
+module.exports = function(sequelize, DataTypes){
+    var Items = sequelize.define("Items", {
+        ItemName: DataTypes.STRING,
+        ItemDescription: DataTypes.STRING,
+        ItemCost: DataTypes.FLOAT,
+        ItemStock: DataTypes.INTEGER
+    });
+
+    Items.associate = function(models) {
+        Items.belongsTo(model.Orders, {
+            foreignKey:{
+                allowNull: false
+            }
+        });
+        Items.belongsTo(model.Products, {
+            foreignKey: {
+                allowNull: false
+            }
+        });
+    };
+    return Items;
+};
