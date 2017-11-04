0e4a5960666a6401d4fd888a7039e8ec6d698662
Anmol Nuggehalli <anuggehalli93@gmail.com>
2017-11-04 14:06:01 -0600

    most recent application 2:05 pm 11/4/2017

---
+++ db/schema.sql
@@ -0,0 +1,25 @@
+CREATE DATABASE EnergyCalcs;
+
+create table Users(
+    ID int auto_increment not null,
+    Name varchar(50) not null,
+    Email varchar(150) not null,
+	Password varchar(50) not null,
+    primary key(ID)
+);
+
+create table cost(
+    
+);
+
+CREATE TABLE(
+
+);
+
+CREATE TABLE(
+    
+);
+
+create table orders(
+    ID 
+);
