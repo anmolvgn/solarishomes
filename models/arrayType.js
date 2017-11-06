4fc1d6c4dabf25a9b82d2fc4f1f3f46134acd59d
Gabe Gebremariam <gebremar@colorado.edu>
2017-11-05 23:44:22 -0700

    Update arrayType.js

--- models/arrayType.js
+++ models/arrayType.js
@@ -1,9 +1,10 @@
 //create table with options and description
 
+
 module.exports = function(sequelize, DataTypes) {
     var Mounts = sequelize.define("Mounts", {
         Options: DataTypes.INTEGER,
         Description: DataTypes.STRING
     });
     return Mounts;
-};
+}
